7fd2667654274f2189e14287193b5189a6925481
cancu <juancruz.cancu@gmail.com>
2024-01-09 16:54:04 -0300

    first code in Go

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/cancuuu/go-api
 
 go 1.21.5
+
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"os"
+
+	tasks "github.com/cancuuu/go-api/tasks"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	file, err := os.OpenFile("tasks.json", os.O_RDWR, 0666)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	var tasks []tasks.Task
+
+	info, err := file.Stat()
+	errChecking(err)
+
+	fileIsNotEmpty := info.Size() != 0
+
+	if fileIsNotEmpty {
+		// read file
+		fileBytes, err := io.ReadAll(file)
+		errChecking(err)
+
+		// parse bytes to json and save into the tasks array
+		jsonErr := json.Unmarshal(fileBytes, &tasks)
+		errChecking(jsonErr)
+	} else {
+
+	}
+
+}
+
+func errChecking(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
